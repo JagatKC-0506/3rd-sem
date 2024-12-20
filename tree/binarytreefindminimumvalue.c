f4acd70abb18db1d17695d4cbe98497cfb6110f2
JagatKC-0506 <jagatkc0506@gmail.com>
2024-12-20 10:59:10 +0545

    Add files via upload

---
+++ tree/binarytreefindminimumvalue.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int key;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *getNewNode(int val) {
+    struct node *newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->key = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct node *insert(struct node *root, int val) {
+    if (root == NULL)
+        return getNewNode(val);
+    if (root->key < val)
+        root->right = insert(root->right, val);
+    else if (root->key > val)
+        root->left = insert(root->left, val);
+
+    return root;
+}
+
+struct node *findMin(struct node *root) {
+    if (root == NULL)
+        return NULL;
+    else {
+        if (root->left == NULL) {
+            return root;
+        } else {
+            return findMin(root->left);
+        }
+    }
+}
+
+void inorder(struct node *root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("\t %d", root->key);
+    inorder(root->right);
+}
+
+int main() {
+    struct node *root = NULL;
+    root = insert(root, 75);
+    root = insert(root, 95);
+    root = insert(root, 65);
+    root = insert(root, 68);
+    root = insert(root, 35);
+    root = insert(root, 39);
+    root = insert(root, 40);
+    root = insert(root, 7);
+    root = insert(root, 13);
+    root = insert(root, 15);
+    root = insert(root, 100);
+    inorder(root);
+    printf("\nthe minimum value is: %d\n", findMin(root)->key);
+    return 0;
+}
